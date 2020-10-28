6f23a94724bbc71e8351dfadf9540ccbd7c3041d
Zachary Kerr <zacharykerr@ufl.edu>
2020-10-28 14:10:31 -0400

    Fixed Order of Operations
    Resolved Index out of Bounds for edge cases of having one string being empty

--- Tool.java
+++ Tool.java
@@ -125,18 +125,15 @@ public class Tool {
         int curr = matrix[matrix.length - 1][matrix[0].length - 1]; //value in the current square
         int col = matrix.length - 1; // current column
         int row = matrix[0].length - 1; // current row
-        boolean nextCol;
-        boolean nextRow;
         int indexRow, indexCol; // location at which operation occurrs in original string
-        
 
 
-        while (col > 0 && row > 0) {//loop until adjacent to the top left square
+
+        while (col > 0 || row > 0) {//loop until adjacent to the top left square
             //ensure no out of bounds via checking to see if next index is greater than zero
-            nextCol = col - 1 >= 0;
-            nextRow = row - 1 >= 0;
+
             // if diagonal square is valid, and is greater than or equal to the other squares, take that square
-            if (nextCol && nextRow && matrix[col - 1][row] >= matrix[col - 1][row - 1] && matrix[col][row - 1] >= matrix[col - 1][row - 1]) {
+            if (row != 0 && col != 0 && matrix[col - 1][row] >= matrix[col - 1][row - 1] && matrix[col][row - 1] >= matrix[col - 1][row - 1]) {
                 row--;
                 col--;
                 //if the characters in that row/col are equal, then no operation is necessary
@@ -144,23 +141,24 @@ public class Tool {
 
                     operations.push("Nothing");
                 } else {
-                   //other wise, we must replace the letter in the row with the letter in the collumn
-                   indexCol = col;
-                   indexRow = row;
-                   operations.push("Replace "+ from.charAt(row) + " at index " + indexRow +  " with " + to.charAt(col) + "." );
+                    //other wise, we must replace the letter in the row with the letter in the collumn
+                    indexCol = col;
+                    indexRow = row;
+                    operations.push("Replace "+ from.charAt(row) + " at index " + indexRow +  " with " + to.charAt(col) + "." );
                 }
-            } else if (nextRow && nextCol && matrix[col][row - 1] >= matrix[col - 1][row]) { // otherwise, if the top square is greater or equal to the left square
+            } else if (row == 0 || col != 0 && matrix[col][row - 1] >= matrix[col - 1][row]) { // otherwise, if the top square is greater or equal to the left square
                 if(curr - 1 == matrix[col - 1][row] || curr == matrix[col - 1][row]){// and the current values is one greater or equal to the value of top left square
                     //select this square and perform an insertion of the column's character
-                    indexCol = col;
-                    operations.push("Insert " + to.charAt(col) + " at index " + indexCol +  ".");
+                    indexCol =  - 1;
+                    operations.push("Insert " + to.charAt(col - 1) + " at index " + indexCol +  ".");
                     col--;
 
                 }
 
-            } else{ //otherwise perform a deletion of the character from the next hiest row
+            } else{ //otherwise perform a deletion of the character from the next highest row
+
                 indexRow = row - 1;
-                operations.push("Delete " + from.charAt(row - 1) + " at index " + indexRow + ".");//
+                operations.push("Delete " + from.charAt(indexRow) + " at index " + indexRow + ".");//
                 row--;
 
             }
@@ -168,7 +166,7 @@ public class Tool {
             curr = matrix[col][row]; //update the value of the square to the current square
 
         }
-        //solve the last step to reach the top left square
+        //solve final step
         if(col == 1 && row == 0){
             indexCol = col - 1;
             operations.push("Insert " + to.charAt(col - 1) +  " at index " + indexCol + ".");
@@ -177,14 +175,13 @@ public class Tool {
             indexRow = row - 1;
             operations.push("Delete " + from.charAt(row - 1) + " at index " + indexRow + "." );
 
+
         }else if(row == 1 && col == 1){
             operations.push("Replace " + from.charAt(row) + " with " + to.charAt(col - 1));
 
         }
 
-
         this.orderOfOperations  = operations;
 
     }
-
 }
